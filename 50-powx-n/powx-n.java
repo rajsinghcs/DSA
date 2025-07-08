ab84618f03b990b36963ce107935ed25429de058
Raj Bahadur Singh <122599400+rajsinghcs@users.noreply.github.com>
2025-07-08 09:34:56 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (10.45%) - LeetSync

---
+++ 50-powx-n/powx-n.java
@@ -0,0 +1,22 @@
+class Solution {
+    public double myPow(double x, int n) {
+        double ans = 1.0;
+        long N = n;
+        if (N < 0) {
+            x = 1.0 / x;
+            N = -N;
+        }
+        while(N > 0){
+            if(N % 2 == 1){
+                ans = ans * x;
+                N = N - 1;
+            }else{
+                x = x * x;
+                N = N/2;
+            }
+        }
+
+    
+        return ans;
+    }
+}
