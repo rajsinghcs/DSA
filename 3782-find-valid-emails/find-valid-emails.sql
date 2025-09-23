68ec5353f8b7c66cb44af86707e8e625c3c76fe3
Raj Bahadur Singh <122599400+rajsinghcs@users.noreply.github.com>
2025-09-24 00:07:13 +0530

    Time: 498 ms (13.89%) | Memory: 0B (100.00%) - LeetSync

--- 3782-find-valid-emails/find-valid-emails.sql
+++ 3782-find-valid-emails/find-valid-emails.sql
@@ -1,4 +1,8 @@
-SELECT user_id,
-       email
-FROM Users
-WHERE email REGEXP '^[A-Za-z0-9_]+@[A-Za-z]+\\.com$';
+-- SELECT user_id,
+--        email
+-- FROM Users
+-- WHERE email REGEXP '^[A-Za-z0-9_]+@[A-Za-z]+\\.com$';
+
+select user_id, email from users
+where   email REGEXP '^[a-zA-Z0-9_]+@[a-zA-Z]+\\.com$'
+order by user_id
