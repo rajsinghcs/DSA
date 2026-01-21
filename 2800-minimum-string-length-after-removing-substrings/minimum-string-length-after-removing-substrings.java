576bd4cfeec9541b4fdc2d304bfe65d27634c1f1
Raj Bahadur Singh <122599400+rajsinghcs@users.noreply.github.com>
2026-01-21 11:59:28 +0530

    Time: 5 ms (61.98%) | Memory: 45.5 MB (18.20%) - LeetSync

---
+++ 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int minLength(String s) {
+        Stack<Character> st = new Stack<>();
+        StringBuilder str = new StringBuilder();
+
+        for(int i=0; i<s.length(); i++){
+            if(!st.isEmpty()){
+                if((s.charAt(i)=='B' && st.peek() =='A') || (s.charAt(i)=='D' && st.peek()=='C')){
+                st.pop();
+                }else{
+                    st.push(s.charAt(i));
+                }
+            }else{
+                st.push(s.charAt(i));
+            }
+        }
+        for(char ch : st){
+            str.append(ch);
+        }
+        return str.length();
+    }
+}
