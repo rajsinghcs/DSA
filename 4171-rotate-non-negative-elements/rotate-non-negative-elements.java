b963bd11fb1c9ab95ca2dd308469ff5ea00c3b39
Raj Bahadur Singh <122599400+rajsinghcs@users.noreply.github.com>
2026-01-25 19:53:42 +0530

    Time: 18 ms (78.47%) | Memory: 248.6 MB (88.76%) - LeetSync

--- 4171-rotate-non-negative-elements/rotate-non-negative-elements.java
+++ 4171-rotate-non-negative-elements/rotate-non-negative-elements.java
@@ -1,3 +1,29 @@
+// class Solution {
+//     public int[] rotateElements(int[] nums, int k) {
+//         ArrayList<Integer> list= new ArrayList();
+//         for(int i=0; i<nums.length; i++){
+//             if(nums[i] >= 0){
+//                 list.add(nums[i]);
+//             }
+//         }
+//         if(list.size() == 0) return nums;
+//         k = k % list.size();
+//         for(int i=0; i< k; i++){
+//             int temp = list.remove(0);
+//             list.add(temp);
+//         }
+//         int l = 0;
+//         for(int i=0; i<nums.length; i++){
+//             if(nums[i] >= 0){
+//                 nums[i] = list.get(l);
+//                 l++;
+//             }
+//         }
+//         return nums;
+//     }
+// }
+
+
 class Solution {
     public int[] rotateElements(int[] nums, int k) {
         ArrayList<Integer> list= new ArrayList();
@@ -7,15 +33,12 @@ class Solution {
             }
         }
         if(list.size() == 0) return nums;
+
         k = k % list.size();
-        for(int i=0; i< k; i++){
-            int temp = list.remove(0);
-            list.add(temp);
-        }
         int l = 0;
         for(int i=0; i<nums.length; i++){
             if(nums[i] >= 0){
-                nums[i] = list.get(l);
+                nums[i] = list.get((l+k)%list.size());
                 l++;
             }
         }
