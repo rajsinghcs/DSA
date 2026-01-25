d23226c3495c8ec53cca467ae15598ba51fdb58b
Raj Bahadur Singh <122599400+rajsinghcs@users.noreply.github.com>
2026-01-25 19:47:44 +0530

    Time: 1045 ms (5.03%) | Memory: 277.6 MB (5.17%) - LeetSync

---
+++ 4171-rotate-non-negative-elements/rotate-non-negative-elements.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] rotateElements(int[] nums, int k) {
+        ArrayList<Integer> list= new ArrayList();
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] >= 0){
+                list.add(nums[i]);
+            }
+        }
+        if(list.size() == 0) return nums;
+        k = k % list.size();
+        for(int i=0; i< k; i++){
+            int temp = list.remove(0);
+            list.add(temp);
+        }
+        int l = 0;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] >= 0){
+                nums[i] = list.get(l);
+                l++;
+            }
+        }
+        return nums;
+    }
+}
