c51c61417893e1a1e5652944ee49f7bf29eb1728
Raj Bahadur Singh <122599400+rajsinghcs@users.noreply.github.com>
2026-02-07 15:22:05 +0530

    Time: 22 ms (67.11%) | Memory: 47.5 MB (94.57%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
@@ -1,19 +1,37 @@
+// class Solution {
+//     public int minimumDeletions(String s) {
+//         Stack<Character> st = new Stack<>();
+//         int c1 = 0;
+
+//         for(char ch : s.toCharArray()){
+//             if(ch == 'b'){
+//                 st.push(ch);
+//             }
+//             else if(ch == 'a' && !st.isEmpty()){
+//                 st.pop();
+//                 c1++;
+//             }
+//         }
+//         return c1;
+//     }
+// }
+
+
 class Solution {
     public int minimumDeletions(String s) {
-        Stack<Character> st = new Stack<>();
-        int c1 = 0;
-        int c2 = 0;
+        int cntB = 0;
+        int ans = 0;
 
-        for(char ch : s.toCharArray()){
-            if(ch == 'b'){
-                st.push(ch);
-                
-            }
-            else if(ch == 'a' && !st.isEmpty()){
-                st.pop();
-                c1++;
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i) == 'b'){
+                cntB++;
+            }else{
+                if(cntB> 0){
+                    cntB--;
+                    ans++;
+                }
             }
         }
-        return c1;
+        return ans;
     }
 }
