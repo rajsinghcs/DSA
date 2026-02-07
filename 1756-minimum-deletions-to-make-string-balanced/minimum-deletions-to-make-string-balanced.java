45865d919e0c1a6b247c4df111bc0616a1d8b7d5
Raj Bahadur Singh <122599400+rajsinghcs@users.noreply.github.com>
2026-02-07 15:18:32 +0530

    Time: 79 ms (16.61%) | Memory: 48.1 MB (16.28%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
@@ -2,10 +2,12 @@ class Solution {
     public int minimumDeletions(String s) {
         Stack<Character> st = new Stack<>();
         int c1 = 0;
+        int c2 = 0;
 
         for(char ch : s.toCharArray()){
             if(ch == 'b'){
                 st.push(ch);
+                
             }
             else if(ch == 'a' && !st.isEmpty()){
                 st.pop();
